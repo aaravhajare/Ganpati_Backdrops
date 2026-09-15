1ff3bde423f2c456d987c32833987cff09197b20
aaravhajare <hajareaarav66@gmail.com>
2026-09-15 10:51:44 +0000

    v1.0.8

--- ganpati-backdrops/src/data/backdrops.js
+++ ganpati-backdrops/src/data/backdrops.js
@@ -43,7 +43,7 @@ export const backdrops = [
     id: 6,
     name: "Ganraya",
     category: "bd4",
-    video: "/videos/backdrop4.mp4",
+    video: "/videos/backdrop3.mp4",
     music: "/music/GanRaya.mp3",
   },
 ];
