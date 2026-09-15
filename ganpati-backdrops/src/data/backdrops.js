9a05e01db6c7bae8577f44c2eac0cef366120a8c
aaravhajare <hajareaarav66@gmail.com>
2026-09-15 11:21:35 +0000

    v1.1.0

--- ganpati-backdrops/src/data/backdrops.js
+++ ganpati-backdrops/src/data/backdrops.js
@@ -1,38 +1,6 @@
 export const backdrops = [
   {
     id: 1,
-    name: "Royal Ganpati",
-    category: "Traditional",
-    video: "/videos/backdrop1.mp4",
-    music: "/music/Shreemant Dagdu Sheth.mp3",
-  },
-
-  {
-    id: 2,
-    name: "Divine Glow",
-    category: "Divine",
-    video: "/videos/backdrop2.mp4",
-    music: "/music/Shreemant Dagdu Sheth.mp3",
-  },
-
-  {
-    id: 3,
-    name: "Golden Darbar",
-    category: "Royal",
-    video: "/videos/backdrop3.mp4",
-    music: "/music/Shreemant Dagdu Sheth.mp3",
-  },
-
-  {
-    id: 4,
-    name: "Temple Lights",
-    category: "Temple",
-    video: "/videos/backdrop4.mp4",
-    music: "/music/GanRaya.mp3",
-  },
-
-  {
-    id: 5,
     name: "Chakra",
     category: "Temple",
     video: "/videos/backdrop5.mp4",
@@ -40,7 +8,7 @@ export const backdrops = [
   },
 
   {
-    id: 6,
+    id: 2,
     name: "Ganraya",
     category: "bd4",
     video: "/videos/backdrop3.mp4",
